ee2635377b59fd9df0bbed5d1cb8453b54a04e2b
Hwangtaehun <r283@naver.com>
2026-05-09 21:32:44 +0900

    업로드

---
+++ Bookmarket/src/main/java/com/springboot/config/LoggingConfig.java
@@ -0,0 +1,20 @@
+package com.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.springboot.interceptor.AuditingInterceptor;
+import com.springboot.interceptor.MonitoringInterceptor;
+
+@Configuration
+public class LoggingConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addInterceptor(new MonitoringInterceptor());
+		registry.addInterceptor(new AuditingInterceptor());
+	}
+	
+}
